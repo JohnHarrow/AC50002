1284da17d9e34034b7b5cf1633d3cc19b5bdee3a
JohnHarrow <52578545+JohnHarrow@users.noreply.github.com>
2024-10-06 14:57:16 +0100

    Updated InnerHTML.js
    Added loop

--- scripts/InnerHTML.js
+++ scripts/InnerHTML.js
@@ -1,7 +1,12 @@
 
 function ChangeHTML(){
 var oldParagraph=document.getElementById('Par1');
-var newParagraph="This is a new paragraph, Paragraph 2";
+var newParagraph="This is a new paragraph, Paragraph 1";
+
+for (let i = 2; i < 11; i++) {
+    newParagraph= newParagraph + "<br>This is a new paragraph, Paragraph " + i;
+}
+
 oldParagraph.innerHTML=newParagraph;
 }
 
